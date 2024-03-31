193880ede9607a4de5ed1566cedfc74461e12768
Richard <richardmrb95@gmail.com>
2024-03-31 17:22:51 +0530

    path update

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -59,5 +59,5 @@ class PrepareBaseModel:
         
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
-        model.save         
+        model.save(path)         
         
