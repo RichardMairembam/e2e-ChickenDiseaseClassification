a9f189ac11a27c3dd682f72c4625d80bfc0deb39
Richard <richardmrb95@gmail.com>
2024-03-31 12:53:21 +0530

    Data Ingestion Completed

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # e2e-ChickenDiseaseClassification
+
+
+
+## Workflows
+
+1. update config.yaml
+2. Upadate secret.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. Update the dvc.yaml
+
+
+ 
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,32 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = DataIngestionPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")  
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
